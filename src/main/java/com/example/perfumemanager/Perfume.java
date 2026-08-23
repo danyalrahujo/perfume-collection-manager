78edd11e4a7d843b1ee4384641a0a2a58d6fa37b
Danyal Rahujo <undeadraghu@gmail.com>
2026-08-23 21:55:58 +0200

    GREEN: Store perfume name

--- src/main/java/com/example/perfumemanager/Perfume.java
+++ src/main/java/com/example/perfumemanager/Perfume.java
@@ -2,4 +2,16 @@ package com.example.perfumemanager;
 
 public class Perfume {
 
+	private String name;
+
+	public Perfume() {
+	}
+
+	public Perfume(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
 }
