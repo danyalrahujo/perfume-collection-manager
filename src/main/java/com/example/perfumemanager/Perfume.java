8096e95f07cabdd1e8b08efab8f3d887d9c3f2f6
Danyal Rahujo <undeadraghu@gmail.com>
2026-08-23 22:23:06 +0200

    GREEN: Store perfume rating

--- src/main/java/com/example/perfumemanager/Perfume.java
+++ src/main/java/com/example/perfumemanager/Perfume.java
@@ -6,6 +6,7 @@ public class Perfume {
 	private String brand;
 	private String fragranceFamily;
 	private int volume;
+	private double rating;
 
 	public Perfume() {
 	}
@@ -32,6 +33,14 @@ public class Perfume {
 		this.volume = volume;
 	}
 
+	public Perfume(String name, String brand, String fragranceFamily, int volume, double rating) {
+		this.name = name;
+		this.brand = brand;
+		this.fragranceFamily = fragranceFamily;
+		this.volume = volume;
+		this.rating = rating;
+	}
+
 	public String getName() {
 		return name;
 	}
@@ -45,9 +54,10 @@ public class Perfume {
 	}
 
 	public int getVolume() {
-
 		return volume;
-
 	}
 
+	public double getRating() {
+		return rating;
+	}
 }
