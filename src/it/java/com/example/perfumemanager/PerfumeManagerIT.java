bf13b3662f7c3e40c58b5e54399f8e98b5f4d8aa
Danyal Rahujo <undeadraghu@gmail.com>
2026-08-25 21:37:18 +0200

    Add integration test setup

---
+++ src/it/java/com/example/perfumemanager/PerfumeManagerIT.java
@@ -0,0 +1,27 @@
+package com.example.perfumemanager;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.example.perfumemanager.repository.InMemoryPerfumeRepository;
+import com.example.perfumemanager.repository.PerfumeRepository;
+
+public class PerfumeManagerIT {
+
+	@Test
+	public void testAddPerfumeAndListPerfumesUsingRealRepository() {
+		PerfumeRepository repository = new InMemoryPerfumeRepository();
+		PerfumeManager perfumeManager = new PerfumeManager(repository);
+
+		Perfume perfume = new Perfume("p001", "Sauvage", "Dior", "Woody", 100, 4.5);
+
+		perfumeManager.addPerfume(perfume);
+
+		List<Perfume> perfumes = perfumeManager.listPerfumes();
+
+		assertTrue(perfumes.contains(perfume));
+	}
+}
